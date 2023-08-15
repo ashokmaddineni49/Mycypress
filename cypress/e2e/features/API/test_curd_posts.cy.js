6fbde0ac001d5ca10fc90176d5f2d9cdef056e20
ashokmaddineni49 <ashokkumarmaddineni@my.unt.edu>
2023-08-15 15:31:32 -0400

    commit

---
+++ cypress/e2e/features/API/test_curd_posts.cy.js
@@ -0,0 +1,28 @@
+let userid = 4459342
+const title ="Hey this my API writing"
+const body="Hey I am body "
+
+describe('Creating the post',()=>{
+    context('Post the Data to public/v2/posts',() =>{
+        it('Test Post Request',()=> {
+            cy.request({method:'POST',
+            url:'https://gorest.co.in/public/v2/posts',
+            headers:{
+                'authorization': 'Bearer ' + Cypress.env('ACCESS_TOKEN'), 
+            },
+            body:{
+                "user_id":userid,
+                "title":title,
+                "body":body
+            }
+        }).then((response)=>{
+            expect(response).to.have.property('status').to.equal(201)
+            expect(response.body).to.have.property('id').to.not.be.oneOf([null, ""])
+            expect(response.body.title).to.equal(title)
+            expect(response.body.body).to.equal("Hey I am body ")
+            userId = response.body.id;
+            cy.task('log', 'Created a new Post with id: ' + userId)
+        })
+        })
+    })
+})
